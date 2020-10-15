cc881a03287529788eeecda018f824a034135af2
Ryan Stewart <ryanstewartalex@gmail.com>
2020-10-15 14:50:48 -0500

    Fix Events

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 const blueSky = getComputedStyle(document.documentElement).getPropertyValue('--skycolor');
 const greenGrass = getComputedStyle(document.documentElement).getPropertyValue('--grasscolor');
 
-$(window).on('load', () =>
+$(window).one('load', () =>
 {
 	$('#introanimation').css({'display': 'block'});
 	// first horizontal bar
-	$('#bar').css({'width': '100%'}).on(transitionendevents, () =>
+	$('#bar').css({'width': '100%'}).one(transitionendevents, () =>
 	{
 		// horizontal bar moves up
-		$('#bar').attr('style', function(i,s) { return (s||'') + `top: ${$('#box1').css('height')} !important;`}).on(transitionendevents, () =>
+		$('#bar').attr('style', function(i,s) { return (s||'') + `top: ${$('#box1').css('height')} !important;`}).one(transitionendevents, () =>
 		{
 			// bottom box turns green
-			$('#box2').css({'background-color': greenGrass}).on(transitionendevents, () =>
+			$('#box2').css({'background-color': greenGrass}).one(transitionendevents, () =>
 			{
 				// top box turns blue
-				$('#box1').css({'background-color': blueSky}).on(transitionendevents, () =>
+				$('#box1').css({'background-color': blueSky}).one(transitionendevents, () =>
 				{
 					// shrink bar
-					$('#bar').css({'opacity' : '0'}).on(transitionendevents, () =>
+					$('#bar').css({'opacity' : '0'}).one(transitionendevents, () =>
 					{
 						//fade in grass
-						$('#overlay').css({'opacity': '1'}).on(transitionendevents, () =>
+						$('#overlay').css({'opacity': '1'}).one(transitionendevents, () =>
 						{
 							// scroll in path
-							$('#path').css({'width': '100%', 'height': '100%'}).on(transitionendevents, () =>
+							$('#path').css({'width': '100%', 'height': '100%'}).one(transitionendevents, () =>
 							{
 								// scroll in trees
-								$('.tree').css({'height': `${parseInt(getComputedStyle(document.documentElement).getPropertyValue('--unitwidth')) * 2}px`}).on(transitionendevents, () =>
+								$('.tree').css({'height': `${parseInt(getComputedStyle(document.documentElement).getPropertyValue('--unitwidth')) * 2}px`}).one(transitionendevents, () =>
 								{
 									// scroll in fence
-									$('#fence').css({'height': `${parseInt(getComputedStyle(document.documentElement).getPropertyValue('--unitwidth'))}px`}).on(transitionendevents, () =>
+									$('#fence').css({'height': `${parseInt(getComputedStyle(document.documentElement).getPropertyValue('--unitwidth'))}px`}).one(transitionendevents, () =>
 									{
 										// fade in sun
-										$('#sun').css({'opacity': '1'}).on(transitionendevents, () =>
+										$('#sun').css({'opacity': '1'}).one(transitionendevents, () =>
 										{
 											// scroll in box shadows
 											$('#box2').css({'box-shadow': '0px -10px 10px rgba(0, 0, 0, .07)'});
